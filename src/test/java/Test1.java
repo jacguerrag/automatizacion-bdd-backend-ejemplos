4f61c84dcd24c9d3bfc5239c6117193cb81190b0
jguerrag <jguerrag@canvia.com>
2024-07-29 06:52:28 -1200

    ejemplos hamcrest api

---
+++ src/test/java/Test1.java
@@ -0,0 +1,13 @@
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static  org.hamcrest.Matchers.*;
+
+
+public class Test {
+    @Test
+    
+}
